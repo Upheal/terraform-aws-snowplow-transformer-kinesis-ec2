75e5405f26d19443a4c3cc2170df2dae10771463
jbeemster <joshua.a.beemster@gmail.com>
2025-10-28 11:37:25 +0100

    Bumped service-ec2 to v0.3.2

--- variables.tf
+++ variables.tf
@@ -65,8 +65,8 @@ variable "iam_permissions_boundary" {
   type        = string
 }
 
-variable "amazon_linux_2_ami_id" {
-  description = "The AMI ID to use which must be based of of Amazon Linux 2; by default the latest community version is used"
+variable "amazon_linux_2023_ami_id" {
+  description = "The AMI ID to use which must be based of of Amazon Linux 2023; by default the latest community version is used"
   default     = ""
   type        = string
 }
